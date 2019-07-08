b92c5a98e6e325f7dd2aa6cd6eab6957b32fa566
West Kraemer <kraemerwest@gmail.com>
2019-07-08 15:23:28 +0200

    first commit for fun with json

---
+++ funWithJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  funWithJSON
+//
+//  Created by West Kraemer on 7/8/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
