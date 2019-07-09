faf827d2188a3366c0c90105da895e535348976e
West Kraemer <kraemerwest@gmail.com>
2019-07-09 17:11:45 +0200

    add json to table view display

--- funWithJSON/ViewController.swift
+++ funWithJSON/ViewController.swift
@@ -22,10 +22,19 @@ class ViewController: UITableViewController {
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
-                
+                parse(json: data)
             }
         }
         
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,8 +43,9 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title Goes Here"
-        cell.detailTextLabel?.text = "Detail Text Goes Here"
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
 
