e7f751ad0ca97ce093d8490a07831b332c96c58f
West Kraemer <kraemerwest@gmail.com>
2019-07-09 16:18:17 +0200

    set up basic funcs for tableview.

--- funWithJSON/ViewController.swift
+++ funWithJSON/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var petitions = [String]()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title Goes Here"
+        cell.detailTextLabel?.text = "Detail Text Goes Here"
+        return cell
+    }
 
 }
 
